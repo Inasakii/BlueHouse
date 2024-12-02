ba9244389ef74d49a74d7a6469e3c6275ac7d64e
Inasakii <palmakenlegarde@gmail.com>
2024-12-02 19:33:10 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6FTeusQo-DoinnxjhfXpNqVKGlAvsw9M",
-  authDomain: "try-led-9d876.firebaseapp.com",
-  databaseURL: "https://try-led-9d876-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "try-led-9d876",
-  storageBucket: "try-led-9d876.appspot.com",
-  messagingSenderId: "824598666827",
-  appId: "1:824598666827:web:5fc6a09d039fc6857077ce"
+  // apiKey: your key
+  // authDomain: "your domain",
+  // databaseURL: "your url",
+  // projectId: "your project ID",
+  // storageBucket: "your storageBucket",
+  // messagingSenderId: "your MessengerID",
+  // appId: "your App ID"
 };
 
 // Initialize Firebase
